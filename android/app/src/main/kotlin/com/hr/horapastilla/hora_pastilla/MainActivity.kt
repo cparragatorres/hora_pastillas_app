d29882e74a2029558a2c4f4a7ec52967736d28ab
cristianASUS <humbertpt14@gmail.com>
2024-10-14 22:02:50 -0500

    feat(firebase): configure Firebase for Android and update build.gradle files
    - Added Firebase configuration for Android.
    - Updated project-level build.gradle to include Google Services plugin.
    - Updated app-level build.gradle to apply Google Services plugin.
    - Added google-services.json file for Firebase integration.

--- android/app/src/main/kotlin/com/hr/horapastilla/hora_pastilla/MainActivity.kt
+++ android/app/src/main/kotlin/com/hr/horapastilla/hora_pastilla/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.hr.horapastilla.hora_pastilla
+package com.hr.horapastilla
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:hora_pastilla/pages/login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  // Asegúrate de que Flutter no ejecute el widget hasta que se complete la inicialización
+  WidgetsFlutterBinding.ensureInitialized(); // Asegúrate de que Flutter esté inicializado
+
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    runApp(const MyApp());
+  } catch (e) {
+    // Manejo de errores: puedes registrar el error o mostrar un mensaje
+    print('Error inicializando Firebase: $e');
+  }
 }
 
 class MyApp extends StatelessWidget {
